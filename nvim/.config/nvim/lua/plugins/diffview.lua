5bde7d66be2d840e9fb7024d2d2cefcc0388d22c
William Bailey <william.bailey@onyxtech.com.au>
2026-08-13 11:01:19 +0800

    Update to diffviewplus fork, add keymaps and other config

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,13 +1,28 @@
 return {
-  'sindrets/diffview.nvim',
+  'dlyongemallo/diffview-plus.nvim',
+  version = "*",
   opts = {
     view = {
       -- Configure the layout and behavior of different types of views.
       merge_tool = {
         -- Config for conflicted files in diff views during a merge or rebase.
-        layout = "diff3_mixed",
+        layout = 'diff3_mixed',
       },
     },
   },
+  keys = {
+    {
+      '<leader>hh',
+      function()
+        if next(require('diffview.lib').views) == nil then
+          vim.cmd('DiffviewOpen')
+        else
+          vim.cmd('DiffviewClose')
+        end
+      end,
+      desc = 'Toggle Diffview'
+    },
+    { '<leader>hm', '<cmd>DiffviewOpen main..HEAD<CR>', desc = 'Diff from main' },
+    { '<leader>hf', '<cmd>DiffviewFileHistory %',       desc = 'Current file history' },
+  },
 }
-
