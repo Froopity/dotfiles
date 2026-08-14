8536b2070b461949a29c5ebeeb479f2a46f6467f
froopity <web@frooplet.com>
2026-08-14 09:57:01 +0800

    Improve gitsigns keybindings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 vim.keymap.set('i', 'jk', '<Esc>')
 
 -- Terminal shortcut and exit with esc
-vim.keymap.set('n', '<leader>t', ':terminal<CR>')
+-- vim.keymap.set('n', '<leader>t', ':terminal<CR>') -- Conflicts with gitsigns, not really used.
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
 -- Unbind cmd-line window to stop accidental invocation. Use :<C-f> to open.
@@ -81,64 +81,64 @@ vim.keymap.set('i', '<RightMouse>', '<C-r>+', { desc = 'Right click to paste' })
 -- Remove items from quickfix list.
 -- `d` to delete Visual selection
 local function delete_qf_items()
-    local mode = vim.api.nvim_get_mode()['mode']
+  local mode = vim.api.nvim_get_mode()['mode']
 
-    local start_idx
-    local count
+  local start_idx
+  local count
 
-    if mode == 'n' then
-        -- Normal mode
-        start_idx = vim.fn.line('.')
-        count = vim.v.count > 0 and vim.v.count or 1
-    else
-        -- Visual mode
-        local v_start_idx = vim.fn.line('v')
-        local v_end_idx = vim.fn.line('.')
+  if mode == 'n' then
+    -- Normal mode
+    start_idx = vim.fn.line('.')
+    count = vim.v.count > 0 and vim.v.count or 1
+  else
+    -- Visual mode
+    local v_start_idx = vim.fn.line('v')
+    local v_end_idx = vim.fn.line('.')
 
-        start_idx = math.min(v_start_idx, v_end_idx)
-        count = math.abs(v_end_idx - v_start_idx) + 1
+    start_idx = math.min(v_start_idx, v_end_idx)
+    count = math.abs(v_end_idx - v_start_idx) + 1
 
-        -- Go back to normal
-        vim.api.nvim_feedkeys(
-            vim.api.nvim_replace_termcodes(
-                '<esc>', -- what to escape
-                true, -- Vim leftovers
-                false, -- Also replace `<lt>`?
-                true -- Replace keycodes (like `<esc>`)?
-            ),
-            'x', -- Mode flag
-            false -- Should be false, since we already `nvim_replace_termcodes()`
-        )
-    end
+    -- Go back to normal
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes(
+        '<esc>',         -- what to escape
+        true,            -- Vim leftovers
+        false,           -- Also replace `<lt>`?
+        true             -- Replace keycodes (like `<esc>`)?
+      ),
+      'x',               -- Mode flag
+      false              -- Should be false, since we already `nvim_replace_termcodes()`
+    )
+  end
 
-    local qflist = vim.fn.getqflist()
+  local qflist = vim.fn.getqflist()
 
-    for _ = 1, count, 1 do
-        table.remove(qflist, start_idx)
-    end
+  for _ = 1, count, 1 do
+    table.remove(qflist, start_idx)
+  end
 
-    vim.fn.setqflist(qflist, 'r')
-    vim.fn.cursor(start_idx, 1)
+  vim.fn.setqflist(qflist, 'r')
+  vim.fn.cursor(start_idx, 1)
 end
 
 vim.api.nvim_create_autocmd('FileType', {
-    group = custom_group,
-    pattern = 'qf',
-    callback = function()
-        -- Do not show quickfix in buffer lists.
-        vim.api.nvim_buf_set_option(0, 'buflisted', false)
+  group = custom_group,
+  pattern = 'qf',
+  callback = function()
+    -- Do not show quickfix in buffer lists.
+    vim.api.nvim_buf_set_option(0, 'buflisted', false)
 
-        -- Escape closes quickfix window.
-        vim.keymap.set(
-            'n',
-            '<ESC>',
-            '<CMD>cclose<CR>',
-            { buffer = true, remap = false, silent = true }
-        )
+    -- Escape closes quickfix window.
+    vim.keymap.set(
+      'n',
+      '<ESC>',
+      '<CMD>cclose<CR>',
+      { buffer = true, remap = false, silent = true }
+    )
 
-        -- `dd` deletes an item from the list.
-        vim.keymap.set('n', 'dd', delete_qf_items, { buffer = true })
-        vim.keymap.set('x', 'd', delete_qf_items, { buffer = true })
-    end,
-    desc = 'Quickfix tweaks',
+    -- `dd` deletes an item from the list.
+    vim.keymap.set('n', 'dd', delete_qf_items, { buffer = true })
+    vim.keymap.set('x', 'd', delete_qf_items, { buffer = true })
+  end,
+  desc = 'Quickfix tweaks',
 })
